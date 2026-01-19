252b7489c4a9670834f126fa2069163d6321fde4
AnyamahNdidi <anyamahedwin@gmail.com>
2026-01-19 09:56:00 +0100

    lkdfm

--- src/config/database.ts
+++ src/config/database.ts
@@ -9,6 +9,7 @@ console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_HOST:', process.env.DB_HOST);
 console.log('DB_PORT:', process.env.DB_PORT);
 console.log('DB_PASSWORD length:', process.env.DB_PASSWORD?.length);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 import path from 'path';
 import { logger } from '../utils/logger';
 
