ef1634edbe2625e090999eaf447a4d9a691616e2
AnyamahNdidi <anyamahedwin@gmail.com>
2026-01-19 09:50:42 +0100

    upate

--- src/config/database.ts
+++ src/config/database.ts
@@ -8,6 +8,7 @@ console.log('DB_NAME:', process.env.DB_NAME);
 console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_HOST:', process.env.DB_HOST);
 console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_PASSWORD length:', process.env.DB_PASSWORD?.length);
 import path from 'path';
 import { logger } from '../utils/logger';
 
