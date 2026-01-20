9298e8d6baef712197986ce2717f347421b61a9e
AnyamahNdidi <anyamahedwin@gmail.com>
2026-01-20 01:04:38 +0100

    dsssssdfdfdddhdddghjkj

--- src/config/database.ts
+++ src/config/database.ts
@@ -32,7 +32,7 @@ import { logger } from '../utils/logger';
 const dbConfig = {
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: 'strongin123',
   host: process.env.DB_HOST || '127.0.0.1',
   port: parseInt(process.env.DB_PORT || '5432'),
   dialect: 'postgres' as const,
